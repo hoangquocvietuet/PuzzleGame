ecfd89c0dd2e0989a459e99a9558585d4dad0b8f
hoangquocvietuet@gmail.com <hoangquocvietuet@gmail.com>
2023-03-07 17:03:50 +0700

    update LTexture class

---
+++ LTexture.h
@@ -0,0 +1,50 @@
+#ifndef LTEXTURE_H
+#define LTEXTURE_H
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <string>
+
+class LTexture
+{
+	public:
+		//Initializes variables
+		LTexture();
+
+		//Deallocates memory
+		~LTexture();
+
+		//Loads image at specified path
+        bool loadFromFile(SDL_Renderer* gRenderer, std::string path );
+		
+		//Creates image from font string
+		bool loadFromRenderedText(TTF_Font* gFont, SDL_Renderer* gRenderer, std::string textureText, SDL_Color textColor );
+
+		//Deallocates texture
+		void free();
+
+		//Set color modulation
+		void setColor( Uint8 red, Uint8 green, Uint8 blue );
+
+		//Set blending
+		void setBlendMode( SDL_BlendMode blending );
+
+		//Set alpha modulation
+		void setAlpha( Uint8 alpha );
+		
+		//Renders texture at given point
+		void render(SDL_Renderer* myRenderer, int x, int y, SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE );
+
+		//Gets image dimensions
+		int getWidth();
+		int getHeight();
+
+	private:
+		//The actual hardware texture
+		SDL_Texture* mTexture;
+
+		//Image dimensions
+		int mWidth;
+		int mHeight;
+};
+#endif
